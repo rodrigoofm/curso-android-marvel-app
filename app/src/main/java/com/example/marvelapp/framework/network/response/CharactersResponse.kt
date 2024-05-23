954c30d16094340ed3ffdb48e4636ac635cd0d45
Rodrigo Fernandes <f.rodrigo095@gmail.com>
2024-05-23 08:35:43 -0300

    Repository and DataSource implementation in framework layer

--- app/src/main/java/com/example/marvelapp/framework/network/response/CharactersResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/CharactersResponse.kt
@@ -1,7 +1,16 @@
 package com.example.marvelapp.framework.network.response
 
+import com.example.core.domain.model.Character
+
 data class CharactersResponse(
   val id: String,
   val name: String,
   val thumbnail: ThumbnailResponse
 )
+
+fun CharactersResponse.toCharacterModel(): Character {
+  return Character(
+    name = this.name,
+    imageUrl = "${this.thumbnail.path}.${this.thumbnail.extensionPath}"
+  )
+}
